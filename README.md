2b3651fb9bf7f943f8ba6f843064ce5cfcc896db
Arnan de Gans <adegans@users.noreply.github.com>
2025-11-26 17:11:32 -0700

    Initial commit
    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Tagcloud 1.0
+
+Add a tag cloud to the main blog page with tags from all child pages (posts)
+
+## How to install an extension
+
+[Download ZIP file](https://github.com/adegans/yellow-tagcloud/archive/refs/heads/main.zip) and copy it into your `system/extensions` folder. [Learn more about extensions](https://github.com/annaesvensson/yellow-update).
+
+## Developer
+
+Arnan de Gans.
---
+++ tagcloud.php
@@ -0,0 +1,81 @@
+<?php
+/*
+ Tagcloud extension
+
+ Add a tag cloud to the main blog page with tags from all child pages (posts)
+
+ Usage: [tagcloud] (This generates a tag of up-to 40 words with font sizes between 12 and 34px)
+ 		[tagcloud 35] (This generates a tag of up-to 35 words)
+ 		[tagcloud 50 10 40] (This generates a tag of up-to 50 words with font sizes between 10 and 40px)
+*/
+
+class YellowTagCloud {
+    const VERSION = "1.0";
+    public $yellow; // access to API
+    
+    // Handle initialisation
+    public function onLoad($yellow) {
+        $this->yellow = $yellow;
+    }
+    
+    // Handle page content element
+    public function onParseContentElement($page, $name, $text, $attributes, $type) {
+        $output = "";
+
+        if($name == "tagcloud" && ($type=="block" || $type=="inline")) {
+			if($this->yellow->extension->isExisting("blog")) {
+				$blog_home = $this->yellow->system->get("blogStartLocation");
+
+				if ($blog_home == "auto") {
+					$pages = $page->getChildren();
+				} else {
+					$pages = $this->yellow->content->index();
+				}
+
+				list($maximum_tags, $minimum_font, $maximum_font) = $this->yellow->toolbox->getTextArguments($text);
+				if (is_string_empty($maximum_tags)) $maximum_tags = 40;
+				if (is_string_empty($minimum_font)) $minimum_font = 12;
+				if (is_string_empty($maximum_font)) $maximum_font = 34;
+
+				$tags = array()	;
+				foreach ($pages as $key => $collection) {
+					$page_tags = $collection->get("tag");
+					if(!empty($page_tags)) {
+						$page_tags = explode(",", $page_tags);
+
+						foreach($page_tags as $tag) {
+							$tag = trim($tag);
+							if(!array_key_exists($tag, $tags)) {
+								$tags[$tag] = 1;
+							} else { 
+								$tags[$tag] = $tags[$tag]+1;
+							}
+						}
+					}
+				}
+				
+				$max_qty = max(array_values($tags));
+				$min_qty = min(array_values($tags));
+				$spread = $max_qty - $min_qty;
+				if($spread == 0) $spread = 1;
+				$step = ($maximum_font - $minimum_font)/($spread);
+				
+				arsort($tags, SORT_NUMERIC);
+				$tags = array_slice($tags, 0, $maximum_tags, true);
+				ksort($tags, SORT_STRING);
+
+				$output = "<div class=\"tag-cloud ".htmlspecialchars($name)."\">\n";
+				foreach($tags as $tag => $count) {
+				    $size = $minimum_font + (($count - $min_qty) * $step);
+				    $size = round($size, 2);
+				    $output .= " <a class=\"tag-cloud-link\" href=\"".$page->getLocation(true).$this->yellow->lookup->normaliseArguments("tag:$tag")."\" style=\"font-size:". $size ."px;\">".$tag."</a> ";
+				}
+				$output .= "</div>\n";
+	        } else {
+				$output = "<p style=\"color:#F00;\">Error: Blog extension is not installed!</p>";
+	        }
+	    }
+        return $output;
+    }
+}
+?>
